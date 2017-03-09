7726ffd76796e2de862b2b3bbb4470349dc9ba00
David Lavieri <daviddlavier@gmail.com>
2017-03-09 08:38:55 -0400

    Add config for babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      use: 'babel-loader',
+      test: /\.jsx?$/,
+      exclude: /node_modules/
+    }]
   }
-};
+}
